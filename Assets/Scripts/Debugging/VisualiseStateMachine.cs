0c4eac760a258cb68ceb78b01b01801593c4a235
Charlie Skinner <chazza.phone@gmail.com>
2023-11-20 09:11:36 +0000

    Imported LeanTween, starting working on debugging tools

---
+++ Assets/Scripts/Debugging/VisualiseStateMachine.cs
@@ -0,0 +1,52 @@
+using System;
+using Player;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+namespace Debugging
+{
+    public class VisualiseStateMachine : MonoBehaviour
+    {
+        [Header("Enable Visualisation")][SerializeField]
+        private bool enableVisualisation;
+        [Header("State Images")]
+        [SerializeField] private Image idleState;
+        [SerializeField] private Image airborneState;
+        [SerializeField] private Image jumpingState;
+        [SerializeField] private Image slidingState;
+        [SerializeField] private Image sprintingState;
+        [SerializeField] private Image walkingState;
+        [SerializeField] private Image wallRunState;
+        [SerializeField] private Image wallJumpState;
+        [Header("Input Texts")]
+        [SerializeField] private TextMeshProUGUI movementInputText;
+        [SerializeField] private TextMeshProUGUI jumpPressedText;
+        [SerializeField] private TextMeshProUGUI slidePressedText;
+
+
+        private Canvas _canvas;
+        private PlayerController _playerController;
+        private InputAction _moveAction;
+        private InputAction _jumpAction;
+        private InputAction _slideAction;
+        
+
+
+        private void Update()
+        {
+            if (!enableVisualisation) return;
+            // movementInputText.text = _playerController.
+        }
+
+        private void Start()
+        {
+            if (!enableVisualisation) this.enabled = false;
+            _moveAction = _playerController.playerInput.actions["Movement"];
+            _jumpAction = _playerController.playerInput.actions["Jump"];
+            _slideAction = _playerController.playerInput.actions["Slide"];
+
+        }
+    }
+}
